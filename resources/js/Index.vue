31338fbeb7393f2fc44cf2bf1e0c5b1624247934
hazimmazley <hazimmazley@gmail.com>
2022-01-09 23:44:01 +0800

    first commit

---
+++ resources/js/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav class="navbar bg-white border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">Laravel BnB</router-link>
+            <router-link class="btn nav-buuton" :to="{name: 'home'}">Second</router-link>
+        </nav>
+
+        <div class="container mt-4 mb-4 pr-4 pl-4">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
