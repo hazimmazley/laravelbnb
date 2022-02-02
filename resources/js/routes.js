c72181e3ddf1b1e01e11f5091e0468cf22945bd5
hazimmazley <hazimmazley@gmail.com>
2022-02-02 09:41:29 +0800

    review page component markup and route

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables.vue";
 import Bookable from "./bookable/Bookable.vue"
+import Review from "./review/Review.vue"
 
 
 const routes = [
@@ -14,6 +15,13 @@ const routes = [
         component: Bookable,
         name: "bookable"
     },
+    {
+        path: "/review/:id",
+        component: Review,
+        name: "review"
+    },
+
+
 ];
 
 const router = new VueRouter({
