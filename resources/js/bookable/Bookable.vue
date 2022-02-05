e98d9367540e8271285c8f04ba1bac25002acf98
hazimmazley <hazimmazley@gmail.com>
2022-02-05 14:16:57 +0800

    add set basket

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -65,19 +65,18 @@ export default {
         }
     },
 
-    computed: mapState({
-        lastSearch: "lastSearch",
-        inBasketAlready(state) {
+    computed: {
+        ...mapState({
+            lastSearch: "lastSearch",
+        }),
+
+        inBasketAlready() {
             if (this.bookable == null) {
                 return false
             }
-
-            return state.basket.items.reduce(
-                (result, item) => result || item.bookable.id == this.bookable.id, 
-                false
-            );
+            return this.$store.getters.inBasketAlready(this.bookable.id)
         }
-    }),
+    },
 
     created() {
         this.loading = true
@@ -106,7 +105,7 @@ export default {
         },
 
         addToBasket() {
-            this.$store.commit("addToBasket", {
+            this.$store.dispatch("addToBasket", {
                 bookable: this.bookable,
                 price: this.price,
                 dates: this.lastSearch
@@ -114,7 +113,7 @@ export default {
         },
 
         removeFromBasket() {
-            this.$store.commit("removeFromBasket", this.bookable.id)
+            this.$store.dispatch("removeFromBasket", this.bookable.id)
         }
     }
 }
--- resources/js/store.js
+++ resources/js/store.js
@@ -20,6 +20,10 @@ export default {
 
         removeFromBasket(state, payload) {
             state.basket.items = state.basket.items.filter(item => item.bookable.id != payload)
+        },
+
+        setBasket(state, payload) {
+            state.basket = payload
         }
     },
 
@@ -34,9 +38,35 @@ export default {
             if (lastSearch) {
                 context.commit('setLastSearch', JSON.parse(lastSearch))
             }
+
+            const basket = localStorage.getItem('basket');
+
+            if(basket) {
+                context.commit('setBasket', JSON.parse(basket))
+            }
+        },
+
+        addToBasket({ commit, state}, payload) {
+            commit('addToBasket', payload);
+
+            localStorage.setItem('basket', JSON.stringify(state.basket))
+            
+        },
+        removeFromBasket({commit, state}, payload) {
+            commit('removeFromBasket', payload);
+
+            localStorage.setItem('basket', JSON.stringify(state.basket))
         }
     },
     getters: {
-        itemsInBasket: (state) => state.basket.items.length
+        itemsInBasket: (state) => state.basket.items.length,
+        inBasketAlready(state) {
+            return function(id) {
+                return state.basket.items.reduce(
+                    (result, item) => result || item.bookable.id == id, 
+                    false
+                );
+            }
+        }
     }
 }
